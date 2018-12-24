67ed20dd0c79527c0d0ebd505bfe26f9a689df59
wips <wips@wips-PC>
2018-12-24 14:23:57 +0900

    test js1

---
+++ homeJS.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+function test() {
+	console.log('1');
+	var test123=new SudokuData(1);
+}
+
+public class SudokuData {
+	private int answer;
+	private int banNumber;
+
+	public SudokuData(int answer) {
+		super();
+		this.answer = answer;
+	}
+
+	public int getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(int answer) {
+		this.answer = answer;
+	}
+
+	public int getBanNumber() {
+		return banNumber;
+	}
+
+	public void setBanNumber(int i) {
+		this.banNumber = i;
+	}
+
+}
--- myJS.js
+++ myJS.js
@@ -5,7 +5,3 @@ function resizeIframe() {
 	iframeHeight = document.getElementById("section").contentWindow.document.body.scrollHeight + 35;
 	document.getElementById("section").height = iframeHeight;
 }
-
-function test() {
-	console.log('1');
-}
