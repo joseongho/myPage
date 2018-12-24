149bfec750a6ab726b715b8927e763cc615b0b1a
wips <wips@wips-PC>
2018-12-24 13:54:29 +0900

    test JS DTO

--- myJS.js
+++ myJS.js
@@ -1,7 +1,39 @@
 /**
  * 
  */
+public class SudokuData {
+	private int answer;
+	private int banNumber;
+
+	public SudokuData(int answer) {
+		super();
+		this.answer = answer;
+	}
+
+	public int getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(int answer) {
+		this.answer = answer;
+	}
+
+	public int getBanNumber() {
+		return banNumber;
+	}
+
+	public void setBanNumber(int i) {
+		this.banNumber = i;
+	}
+
+}
+
 function resizeIframe() {
 	iframeHeight = document.getElementById("section").contentWindow.document.body.scrollHeight + 35;
 	document.getElementById("section").height = iframeHeight;
+}
+
+function test(){
+	var test= new SudokuData(1);
+	console.log(test.getAnswer);
 }
