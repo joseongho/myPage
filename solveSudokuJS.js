f649adfd8083cbeeae815b9bda6e480538b11c46
wips <wips@wips-PC>
2018-12-24 16:16:24 +0900

    test

--- solveSudokuJS.js
+++ solveSudokuJS.js
@@ -1,3 +1,17 @@
 /**
  * 
  */
+function solve() {
+    var test = [];
+    for (var i = 0; i < 81; i++) {
+        test.push(sudokuDate);
+        test[i].answer = document.getElementsByName("n" + i).values;
+    }
+    console.log(test.length);
+}
+
+var sudokuDate = {
+    answer: "",
+    banNumber: ""
+
+}
